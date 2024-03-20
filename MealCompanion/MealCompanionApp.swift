80e60c736dcd307eac43d381c749009d1929280d
Woojin <woojinlee87@gmail.com>
2024-03-20 09:36:11 +0900

    Create SwiftUI Project

---
+++ MealCompanion/MealCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealCompanionApp.swift
+//  MealCompanion
+//
+//  Created by Woojin on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MealCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
